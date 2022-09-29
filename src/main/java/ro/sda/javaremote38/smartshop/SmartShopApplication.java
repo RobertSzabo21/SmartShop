2264ac635082d55d723a9a77858fef3f3fda5350
Robert21S <robert21szabo@gmail.com>
2022-09-29 19:23:24 +0300

    Initial commit

---
+++ src/main/java/ro/sda/javaremote38/smartshop/SmartShopApplication.java
@@ -0,0 +1,13 @@
+package ro.sda.javaremote38.smartshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/sda/javaremote38/smartshop/SmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sda.javaremote38.smartshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
